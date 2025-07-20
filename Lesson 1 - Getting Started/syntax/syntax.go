10423e54232bf5c70046fefe865ed1cbbad98a2a
nhatquangpx <doannhatquang0@gmail.com>
2025-07-20 22:04:17 +0700

    EX1, 2, 3

---
+++ Lesson 1 - Getting Started/syntax/syntax.go
@@ -0,0 +1,19 @@
+// Cú pháp:
+// Một file Go bao gồm các phần sau:
+// - Package declaration: Khai báo package của file.
+// - Import packages: Khai báo các package cần sử dụng
+// - Functions: Các hàm được định nghĩa trong file.
+// - Statements and expressions: Các câu lệnh và biểu thức thực thi trong hàm.
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("This is Syntax lesson in Go")
+
+}
+
+// Các câu lệnh được phân tach bằng dấu xuống dòng hoặc dấu chấm phẩy.
+// Nhấn phím Enter sẽ ngầm thêm dấu chấm phẩy vào cuối câu lệnh, nhưng không hiển thị trong mã nguồn
+// Dấu ngoặc nhọn trái không thể đứng đầu dòng
---
+++ Lesson 1 - Getting Started/variable/variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Có 2 cách để khai báo biến
+	// Cách 1: Khai báo biến với từ khóa var
+	// var variablename type = value
+	var name string = "Nguyen Van A"
+	// Cach 2: Khai báo biến với từ khóa :=
+	// variablename := value
+	
+}
