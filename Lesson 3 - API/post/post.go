d128704a2b12cf42f0b932304f187f2697adfcea
nhatquangpx <doannhatquang0@gmail.com>
2025-07-24 15:07:04 +0700

    Validate

--- Lesson 3 - API/post/post.go
+++ Lesson 3 - API/post/post.go
@@ -2,8 +2,10 @@ package post
 
 type Post struct {
 	ID      int    `json:"id"`
-	Title   string `json:"title"`
-	Content string `json:"content"`
+	Title   string `json:"title" validate:"required, min=3"`	// Thêm tag validate tối thiểu 3 ký tự
+	Slug    string `json:"slug" validate:"required"`	// Thêm tag validate bắt buộc không được rỗng
+	Content string `json:"content" validate:"required"`
+	AuthorID int   `json:"author_id"` // Chuẩn bị cho phần login
 }
 
 var posts = []Post{}
