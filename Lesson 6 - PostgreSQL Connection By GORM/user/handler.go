d8de4ad25b13d4f306c27002a0c9b80c6641612e
nhatquangpx <doannhatquang0@gmail.com>
2025-07-25 16:44:06 +0700

    Initialize project

---
+++ Lesson 6 - PostgreSQL Connection By GORM/user/handler.go
@@ -0,0 +1,89 @@
+package user
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+var jwtSecret = []byte("my_secret_key")
+
+// Register godoc
+// @Summary Đăng ký người dùng mới
+// @Tags Auth
+// @Accept json
+// @Produce json
+// @Param body body user.User true "Thông tin người dùng"
+// @Success 201 {object} map[string]interface{}
+// @Failure 400 {object} map[string]string
+// @Router /register [post]
+func Register(c *fiber.Ctx) error {
+	var req User
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid request body",
+		})
+	}
+
+	hashed, err := bcrypt.GenerateFromPassword([]byte(req.Password), 10)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Failed to hash password",
+		})
+	}
+
+	req.ID = nextID
+	nextID++
+	req.Password = string(hashed)
+	Users = append(Users, req)
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "User registered successfully",
+		"user_id": req.ID,
+	})
+}
+
+func Login(c *fiber.Ctx) error {
+	var req User
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid request body",
+		})
+	}
+	var user User
+	found := false
+	for _, u:= range Users {
+		if u.Username == req.Username {
+			user = u
+			found = true
+			break
+		}
+	}
+
+	if !found || bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)) != nil {
+	// user.Password đã được băm sẵn khi đăng ký, dùng bcrypt.GenerateFromPassword()
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Invalid username or password",
+		})
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": user.ID,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	})
+	// JWT này sẽ được gắn vào các request sau này để xác thực người dùng
+
+	signedToken, err := token.SignedString(jwtSecret)		// Ký token với secret key
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to generate token",
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"token": signedToken,
+	})
+	// Gửi token cho client dưới dạng:
+	//	{ "token": "<chuỗi JWT>" }
+}
